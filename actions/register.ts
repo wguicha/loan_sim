d8e299e735a756d6e88dd589ff9085ff53d3a1fe
wguicha <williamalberto1@hotmail.com>
2024-06-28 22:31:39 -0500

    mod register action

--- actions/register.ts
+++ actions/register.ts
@@ -1,5 +1,14 @@
-"use server";
+"use server"
 
+import { RegisterSchema } from "@/schemas";
 import * as z from "zod";
 
-import { RegisterSchema } from "@/schemas"
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
+    const validatedFields = RegisterSchema.safeParse(values);
+
+    if(!validatedFields.success) {
+        return { error: "Valores de campo invalido"}
+    }
+
+    return { success: "Valores de campo validos"}
+}
