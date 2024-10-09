3b23e22a36a7f57f6d80b3511bc720464eca2ef5
wguicha <williamalberto1@hotmail.com>
2024-10-09 07:57:32 -0500

    middleware with restrictions in logging

--- auth.ts
+++ auth.ts
@@ -2,12 +2,13 @@ import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 
 import { db } from "@/lib/db"
-import authConfig from "./auth.config"
+import authConfig from "@/auth.config"
 
+/*Se cambio para que funcionara: handlers: { GET, POST }*/
 
 export const {
   auth,
-  handlers: { GET, POST },
+  handlers,
   signIn,
   signOut
 } = NextAuth({
--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,40 @@
-import NextAuth from "next-auth"
-import authConfig from "@/auth.config"
+import NextAuth from "next-auth";
+
+import authConfig from "@/auth.config";
+import {
+  DEFAULT_LOGIN_REDIRECT,
+  apiAuthPrefix,
+  authRoutes,
+  publicRoutes
+} from "@/routes";
 
 export const { auth } = NextAuth(authConfig)
 
 export default auth((req) => {
+  const { nextUrl } = req;
   const isLoggedIn = !!req.auth;
-  console.log("ROUTE:", req.nextUrl.pathname);
-  console.log("IS LOGGED IN:", isLoggedIn);
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  if (isApiAuthRoute) {
+    console.log("ESTOY PASANDO AQUI:", isApiAuthRoute)
+    return null;
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
+    }
+
+    return null;
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL("/auth/login", nextUrl));
+  }
+
 })
 
 export const config = {
